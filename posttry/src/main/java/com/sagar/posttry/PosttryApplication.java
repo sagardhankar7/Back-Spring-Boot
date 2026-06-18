4dd68312c25dc04d9b7c46370992105b202edbd3
sagardhankar7 <sag9315@gmail.com>
2026-06-18 18:21:21 +0530

    initial commit

---
+++ posttry/src/main/java/com/sagar/posttry/PosttryApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.posttry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosttryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosttryApplication.class, args);
+	}
+
+}
---
+++ posttry/src/test/java/com/sagar/posttry/PosttryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.posttry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosttryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
